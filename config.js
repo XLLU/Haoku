677aa5a054cb0e9630aae0d13c2b0ee9998c725e
lucas lu <lucasluu@yahoo.com>
2015-04-24 11:40:57 +0800

    Changed Client Id/Secret to specific sfdc org.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.dev = {
-    "clientId": "3MVG9Y6d_Btp4xp5NwcRLAU5Ct6gjXLPoSW43wDXmP0NR5siDu10UMOBMnPip9v02g_emnt1gJetjbnCH9N3y",
-    "clientSecret": "6154018842396108114",
+    "clientId": "3MVG9Y6d_Btp4xp6Es1QfF8oIsgjcjtFHyx7sdhl7V8sh_CINU9v0WOLTBqV1QyXMuIpGcTQuGIcHGrUrtCma",
+    "clientSecret": "8912425315286274910",
     "redirectUri": "http://localhost:3000/oauth/callback"
 };
 
